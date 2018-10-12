4fbff6544445a9a91e5cb47df7796943fc7ada00
MDZZ792 <neat0822@163.com>
2018-10-12 11:42:41 +0800

    加入了多线程练习，去掉了一些错误的注释

--- src/main/java/com/test/Prometheus/Client/PushGatWay.java
+++ src/main/java/com/test/Prometheus/Client/PushGatWay.java
@@ -19,7 +19,6 @@ import java.util.*;
 public class PushGatWay {
     //Gauge仪表盘
     public static void Gauge() throws Exception {
-        //因为同时建了两个表,所以需要CollectorRegistry,只建立一个表时可以省略.
         CollectorRegistry registry = new CollectorRegistry();
 //        Gauge.Timer durationTimer = null;
         //Gauge属性的值可加可减,类似仪表盘
@@ -28,20 +27,21 @@ public class PushGatWay {
         try {
             // Your code here.
             //设定labelValues，字段1的值加100
-            gauge.labels("T_One").inc(100);
+            gauge.labels("T_One_T","T_One_01_T").set(21546);
+            gauge.labels("T_One").inc(10);
             //字段1的值减26
-            gauge.labels("T_One").dec(26);
-            gauge.labels("T_One").dec(10);
-            gauge.labels("T_One_01").dec(300);
-            gauge.labels("T_One_01").inc(500);
+            gauge.labels("T_One").dec(260);
+            gauge.labels("T_One").dec(100);
+            gauge.labels("T_One_01").dec(30);
+            gauge.labels("T_One_01").inc(50);
 //            durationTimer = gauge.startTimer();
             // This is only added to the registry after success,
             // so that a previous success in the Pushgateway isn't overwritten on failure.
             Gauge lastSuccess = Gauge.build()
-                    .name("my_batch2_job_last_success").help("Last time my batch job succeeded, in unixtime.").labelNames("Two").register(registry);
+                    .name("MDZZ_gauge2").help("Last time my batch job succeeded, in unixtime.").labelNames("Two").register(registry);
 //            lastSuccess.setToCurrentTime();
-            lastSuccess.labels("T_Two").dec(500);
-            lastSuccess.labels("T_Two").inc(100);
+            lastSuccess.labels("T_Two").dec(50);
+            lastSuccess.labels("T_Two").inc(10);
         } finally {
 //            durationTimer.setDuration();
             PushGateway pg = new PushGateway("127.0.0.1:9091");
@@ -49,6 +49,8 @@ public class PushGatWay {
             pg.pushAdd(registry, "gauge_job");
 
             System.out.println("--------------------------------"+pg.instanceIPGroupingKey());
+            System.out.println("Push Gauge success!");
+
         }
     }
 
@@ -60,16 +62,14 @@ public class PushGatWay {
         PushGateway pg = new PushGateway("localhost:9091");
         Map<String,String> map=new HashMap<String,String>();
 //        map.put("a","2");
-        while(true) {
             int i=0;
             while (i<30) {
                 Thread.sleep(200);
                 histogram.labels("MDZZ", "2018-09-14", "LBM0001","Test_job")
-                        .observe(new Random().nextInt(5000));
+                        .observe(new Random().nextInt(537));
                 i++;
-            }
             pg.pushAdd(histogram, "histogram_job");
-            System.out.println("Push success!");
+            System.out.println("Push Histogram success!");
         }
     }
 
@@ -82,10 +82,10 @@ public class PushGatWay {
        map.put("WHISTHIS","OVER");
        for(int i=0;i<10;i++){
            Thread.sleep(200);
-           summary.labels("TEST","TESTT","TESTTT").observe(new Random().nextInt(100));
+           summary.labels("TEST","TESTT","TESTTT").observe(new Random().nextInt(9875));
        }
        pg.pushAdd(summary,"summary_job",map);
-       System.out.println("Push success!");
+       System.out.println("Push Summary success!");
    }
 
    //Counter计数器,只能加
@@ -94,11 +94,11 @@ public class PushGatWay {
        Counter counter = Counter.build().name("MDZZ_counter").help("Test Counter").labelNames("TestC01").labelNames("TestC02").register(registry);
        PushGateway pushGatWay = new PushGateway("localhost:9091");
        Map<String, String> map = new HashMap<String, String>();
-       counter.labels("T_testC01").inc();
-       counter.labels("T_testC02").inc(10);
+       counter.labels("T_testC01").inc(52);
+       counter.labels("T_testC02").inc(741);
 
        pushGatWay.pushAdd(registry,"counter_job");
-       System.out.println("Push success!");
+       System.out.println("Push Counter success!");
    }
 
     //没什么卵用的删除
@@ -154,7 +154,7 @@ public class PushGatWay {
     }
 
     //对于传回来的数据的解析
-    public static String X(String s){
+    public static void X(String s){
         JSONObject jsonObject = JSONObject.fromObject(s);
         System.out.println(jsonObject.toString());
         String data = jsonObject.getString("data");
@@ -171,7 +171,7 @@ public class PushGatWay {
             String metric = jsonObject2.getString("metric");
 //            System.out.println("前:"+metric);
             JSONObject JB = JSONObject.fromObject(metric);
-            bean.setOne_01(JB.getString("One_01"));
+            bean.setOne_01(JB.getString("One"));
             bean.setExported_job(JB.getString("exported_job"));
             bean.set__name__(JB.getString("__name__"));
             bean.setInstance(JB.getString("instance"));
@@ -188,7 +188,6 @@ public class PushGatWay {
         }
 
 //        System.out.println(jsonObject1);
-        return "";
     }
 
     /**
@@ -200,11 +199,12 @@ public class PushGatWay {
      *   ...
      * ]
      */
-    public static String Z(String z){
+    public static void Z(String z){
         JSONObject jsonObject = JSONObject.fromObject(z);
         String data = jsonObject.getString("data");
         JSONObject jsonObject1 = JSONObject.fromObject(data);
         String resultType = jsonObject1.getString("resultType");
+        System.out.println(resultType);
         JSONArray result = jsonObject1.getJSONArray("result");
         for(int i=0;i<result.size();i++){
             JSONObject jsonObject2 = JSONObject.fromObject(result.get(i));
@@ -214,13 +214,41 @@ public class PushGatWay {
             for(int j=0;j<values.size();j++){
                 JSONArray ja = (JSONArray) values.get(j);
                 for(int a=0;a<ja.size();a++){
-                    ja.get(a);
+                    Object o = ja.get(a);
+                    String format;
+                    if(o.toString().indexOf("E")>-1){
+                        Double d = Double.parseDouble(o.toString());
+                        format = E(d.toString());
+                        System.out.println(format);
+                    }else
                     System.out.println(ja.get(a));
                 }
             }
         }
-        return "";
     }
+
+    //解决时间为科学计数法
+    public static String E(String e){
+        //保留三位小数点，然并卵
+//        new DecimalFormat("0.000").format(e);
+        BigDecimal bd = new BigDecimal(e);
+        String s1 = bd.toPlainString();
+
+        Long time = new Date().getTime();
+        String format = null;
+        if(time.toString().length() > s1.length()){
+            int i = time.toString().length()- bd.toPlainString().length();
+            for(;0<i;i--)
+                s1 = s1 + "0";
+            Long date = Long.valueOf(s1);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+            format = sdf.format(new Date(date));
+        }else
+            format="核对时间位数";
+        return format;
+    }
+
+
 
     public static void main(String[] agrs){
         try{
@@ -229,7 +257,7 @@ public class PushGatWay {
             Histogram();
             Summary();
             Counter();
-
+/*数据消失原因：代码将数据push到pushgateway中,pushgateway等待primetheus pull.pushgateway相当与内存*/
             /**
              * http://localhost:9090/api/v1/query?query={One_01=~"T_.*"}&start=2018-10-08T09:00:00Z&end=2018-10-08T12:12:12Z
              * 其中, ~"T_.*"为查询所有以T_开头的One_01标签,匹配正则,label之间通过and,or,unless进行组合
@@ -240,26 +268,11 @@ public class PushGatWay {
 
 
             //{"status" : "success","data" : {"resultType" : "matrix","result" : [{"metric" : {"__name__" : "up","job" : "prometheus","instance" : "localhost:9090"},"values" : [[ 1435781430.781, "1" ],[ 1435781445.781, "1" ],[ 1435781460.781, "1" ]]},{"metric" : {"__name__" : "up","job" : "node","instance" : "localhost:9091"},"values" : [[ 1435781430.781, "0" ],[ 1435781445.781, "0" ],[ 1435781460.781, "1" ]]}]}}
-            String z = "{\"status\" : \"success\",\"data\" : {\"resultType\" : \"matrix\",\"result\" : [{\"metric\" : {\"__name__\" : \"up\",\"job\" : \"prometheus\",\"instance\" : \"localhost:9090\"},\"values\" : [[ 1435781430.781, \"1\" ],[ 1435781445.781, \"1\" ],[ 1435781460.781, \"1\" ]]},{\"metric\" : {\"__name__\" : \"up\",\"job\" : \"node\",\"instance\" : \"localhost:9091\"},\"values\" : [[ 1435781430.781, \"0\" ],[ 1435781445.781, \"0\" ],[ 1435781460.781, \"1\" ]]}]}}";
-            Z(z);
+      /*      String z = "{\"status\" : \"success\",\"data\" : {\"resultType\" : \"matrix\",\"result\" : [{\"metric\" : {\"__name__\" : \"up\",\"job\" : \"prometheus\",\"instance\" : \"localhost:9090\"},\"values\" : [[ 1435781430.781, \"1\" ],[ 1435781445.781, \"1\" ],[ 1435781460.781, \"1\" ]]},{\"metric\" : {\"__name__\" : \"up\",\"job\" : \"node\",\"instance\" : \"localhost:9091\"},\"values\" : [[ 1435781430.781, \"0\" ],[ 1435781445.781, \"0\" ],[ 1435781460.781, \"1\" ]]}]}}";
+            Z(z);*/
 
 
-            //解决时间为科学计数法
-            BigDecimal bd = new BigDecimal("1.53898048E9");
-            System.out.println(bd.toPlainString());
-            String s1 = bd.toPlainString();
-            Long time = new Date().getTime();
-            System.out.println(new Date().getTime());
-            if(time.toString().length() > s1.length()){
-               int i = time.toString().length()- bd.toPlainString().length();
-               for(;0<i;i--){
-                   s1 = s1+"0";
-               }
-               Long date = Long.valueOf(s1);
-               SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
-               String format = sdf.format(new Date(date));
-               System.out.println(format);
-            }
+
         }catch (Exception e){
             e.printStackTrace();
         }
